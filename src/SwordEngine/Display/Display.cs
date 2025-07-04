f3881748eaadbbc15ead37d8477c5f1eb86a124d
Chavez, Gustavo N. <gustavo.chavez144@outlook.com>
2025-07-04 16:28:39 -0500

    Modified main display class.
    Added additional comment, code clean up.

--- src/SwordEngine/Display/Display.cs
+++ src/SwordEngine/Display/Display.cs
@@ -1,18 +1,27 @@
 ﻿using SkiaSharp;
-using System.Drawing;
-using System.Numerics;
-using System.Runtime.Versioning;
 
 namespace SwordEngine.Display
 {
     public class Display
     {
+        /// <summary>
+        /// The frame of the window to render.
+        /// </summary>
         private SKSurface frame { get; set; }
 
+        /// <summary>
+        /// The canvas of the displayed window, inside the borders.
+        /// </summary>
         private SKCanvas canvas { get; set; }
 
+        /// <summary>
+        /// The hidden height.
+        /// </summary>
         private int height;
 
+        /// <summary>
+        /// The hidden width.
+        /// </summary>
         private int width;
 
         /// <summary>
@@ -20,15 +29,22 @@ namespace SwordEngine.Display
         /// </summary>
         private String title;
 
-        public Display(string title, int width, int height) 
+        /// <summary>
+        /// The generic Display instantiation method.
+        /// </summary>
+        /// <param name="width">The desired game width.</param>
+        /// <param name="height">The desired game height.</param>
+        public Display(int width, int height) 
         {
-            this.title = title;
             this.width = width;
             this.height = height;
 
             createDisplay();
         }
 
+        /// <summary>
+        /// Creates a Window Display in the Operating System utilizing SkiaSharp.
+        /// </summary>
         private void createDisplay()
         {
             // First determine Frame size.
@@ -36,14 +52,18 @@ namespace SwordEngine.Display
             imageInfo.Width = width;
             imageInfo.Height = height;
 
-            this.frame = SKSurface.Create(imageInfo);
+            frame = SKSurface.Create(imageInfo);
 
             // Create an empty bitmap to display
             SKBitmap bitmap = new SKBitmap(width, height);
 
             // Get the canvas now from the newly created frame.
-            SKCanvas canvas = this.frame.Canvas;
+            SKCanvas canvas = frame.Canvas;
             canvas.Clear(SKColors.White);
+
+            // Draw a point and render the bitmap.
+            SKPoint point = new SKPoint(0, 0);
+            canvas.DrawBitmap(bitmap, point);
 
             this.canvas = canvas;
         }
@@ -53,16 +73,15 @@ namespace SwordEngine.Display
         /// </summary>
         public SKCanvas getCanvas()
         {
-            return this.canvas;
+            return canvas;
         }
-
 
         /// <summary>
         /// Returns the SKSurface.
         /// </summary>
         public SKSurface getSurface()
         {
-            return this.frame;
+            return frame;
         }
     }
 }
